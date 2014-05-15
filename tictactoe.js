e2c392bac0c321350387bccd50fc043be4081a80
Tyler Boright <tyler.boright@gmail.com>
2014-05-14 15:19:16 -1000

    added players to javascript file and instantiated rules object

---
+++ tictactoe.js
@@ -0,0 +1,12 @@
+// Object containing rules
+var Model = {};
+
+var player_1 = {
+  name: "Player 1",  //Default name 
+  symbol: "X"
+};
+
+var player_2 = {
+  name: "Player 2",  //Default name 
+  symbol: "O"
+};
