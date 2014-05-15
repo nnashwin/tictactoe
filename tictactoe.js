644a11b2d48b75f0ebe0ea5fc11c0fe4700ce50a
Tyler Boright <tyler.boright@gmail.com>
2014-05-14 17:42:17 -1000

    added win lines to Model method and integrating other turns

--- tictactoe.js
+++ tictactoe.js
@@ -1,12 +1,30 @@
 // Object containing rules
-var Model = {};
+var Model = {
+  players: [
+    {
+      name: "Player 1",  //Default name 
+      symbol: "X",
+      fields: []
+    },
+    {
+      name: "Player 2",  //Default name 
+      symbol: "O",
+      fields: []
+    }
+  ],
+  turn_no : 0,
+  current_player: {},
+  winLines: [
+    [1,2,3], [4,5,6], [7,8,9],
+    [1,4,7], [2,5,8], [3,6,9],
+    [1,5,9], [3,5,7]
+  ]
+  nextTurn: function() {
+    Model.current_player = Model.players[Model.turn_no % 2];
 
-var player_1 = {
-  name: "Player 1",  //Default name 
-  symbol: "X"
-};
 
-var player_2 = {
-  name: "Player 2",  //Default name 
-  symbol: "O"
+    };
+
+    Model.turn_no += 1;
+  }
 };
